d829d0a4e68f8ec23a2314ca2657972264a705b5
Tyler Neylon <tylerneylon@gmail.com>
2014-10-08 21:22:45 -0700

    Be able to parse products.

--- parse.lua
+++ parse.lua
@@ -14,12 +14,12 @@
 Here's the idea behind how we'll parse expressions.
 
 expr -> sum
-sum -> prod[ + prod]
-prod -> num[ * num]
+sum -> prod[ + prod]*
+prod -> num[ * num]*
 num -> "\d+"
 
 On success, each parse_X function will return tree, tail, where tree is
-an abstract syntax tree of the form {type, kids, value}, and tail is
+an abstract syntax tree of the form {type, (kids|value)}, and tail is
 the unparsed portion of the string.
 
 On failure, parse_X returns 'no match', tail, where tail is the full
@@ -40,10 +40,23 @@ function parse_sum(sum_str)
 end
 
 function parse_prod(prod_str)
+  local tree = {type='prod', kids={}}
+  local result, tail = parse_num(prod_str)
+  if result == 'no match' then return result, prod_str end
+  tree.kids[1] = result
+  local first, last = tail:find('^%s*%*%s*')
+  while first do
+    tail = tail:sub(last + 1)
+    result, tail = parse_num(tail)
+    if result == 'no match' then return result, prod_str end
+    tree.kids[#tree.kids + 1] = result
+    first, last = tail:find('^%s*%*%s*')
+  end
+  return tree, tail
 end
 
 function parse_num(num_str)
-  first, last = num_str:find('^%d+')
+  local first, last = num_str:find('^%d+')
   if not first then return 'no match', num_str end
   local value = tonumber(num_str:sub(first, last))
   return {type='num', value=value}, num_str:sub(last + 1)
@@ -54,13 +67,21 @@ end
 -- Debug functions.
 ------------------------------------------------------------------------------
 
--- TODO Fill out this function or copy over another similar already done one.
-function pr_val(val)
-  if type(val) == 'table' then
-    for k, v in pairs(val) do print(k, v) end
+function pr(obj, indent)
+  indent = indent or ''
+  if type(obj) ~= 'table' then
+    print(indent .. tostring(obj))
     return
   end
-  print(val)
+  for k, v in pairs(obj) do
+    io.write(indent)
+    if type(v) == 'table' then
+      print(k .. ':')
+      pr(v, '  ' .. indent)
+    else
+      print(string.format('%-6s = ', k) .. tostring(v))
+    end
+  end
 end
 
 
@@ -80,11 +101,12 @@ local f = assert(io.open(in_file, 'r'))
 for line in f:lines() do
   -- Eventually this will just be:
   --parse_expr(line)
-  local result, tail = parse_num(line)
+  local result, tail = parse_prod(line)
   print('')
-  print('result, tail :')
-  pr_val(result)
-  pr_val(tail)
+  print('result:')
+  pr(result)
+  print('tail:')
+  pr(tail)
 end
 f:close()
 
