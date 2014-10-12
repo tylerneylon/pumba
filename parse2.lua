ff24d9723b3ec808f34fa00c09fce15505a21041
Tyler Neylon <tylerneylon@gmail.com>
2014-10-12 08:19:21 -0700

    Add initial parsing functions for simple grammar.
    They are buggy for now. I have a TODO item in parse2.lua to fix that!

--- parse2.lua
+++ parse2.lua
@@ -11,6 +11,10 @@
 
 --[[
 
+TODO
+ * The current code does not correctly parse all lines. Fix that.
+ * Add the exec style functions, and update comments accordingly.
+
 Here is an informal description of the grammar:
 
 statement -> assign | for | print
@@ -44,49 +48,102 @@ string given as input to parse_X.
 
 
 ------------------------------------------------------------------------------
+-- Metaparse functions.
+------------------------------------------------------------------------------
+
+function parse_or_rule(str, rule_name, or_parsers)
+  local tree = {type = rule_name, kids={}}
+  for _, subparse in ipairs(or_parsers) do
+    local subtree, tail = subparse(str)
+    if subtree ~= 'no match' then
+      tree.kids[#tree.kids + 1] = subtree
+      return tree, tail
+    end
+  end
+  return 'no match', str
+end
+
+function parse_seq_rule(str, rule_name, seq_parsers)
+  local tree = {type = rule_name, kids = {}}
+  local subtree, tail = nil, str
+  for _, subparse in ipairs(seq_parsers) do
+    subtree, tail = subparse(tail)
+    if subtree == 'no match' then return 'no match', str end
+    tree.kids[#tree.kids + 1] = subtree
+  end
+  return tree, tail
+end
+
+function parse_lit(lit_str, name)
+  name = name or '<lit>'
+  local re = '%s*(' .. escaped_lit(lit_str) .. ')'
+  return function(str)
+    local s, e, val = str:find(re)
+    if s == nil then return 'no match', str end
+    return {type = name, value = val}, str:sub(e + 1)
+  end
+end
+
+function parse_re(re_str, name)
+  name = name or '<re>'
+  local re = '%s*(' .. re_str .. ')'
+  return function(str)
+    local s, e, val = str:find(re)
+    if s == nil then return 'no match', str end
+    return {type = name, value = val}, str:sub(e + 1)
+  end
+end
+
+function escaped_lit(lit_str)
+  return lit_str:gsub('[^A-Za-z]', '%%%0')
+end
+
+
+------------------------------------------------------------------------------
 -- Parse functions.
 ------------------------------------------------------------------------------
 
 -- As a near-future step, it might be nice to factor all of these out so they
 -- mainly rely on a single parsing mechanism.
 
-function parse_sum(sum_str)
-  local tree = {type='sum', kids={}}
-  local result, tail = parse_prod(sum_str)
-  if result == 'no match' then return result, sum_str end
-  tree.kids[1] = result
-  local first, last = tail:find('^%s*%+%s*')
-  while first do
-    tail = tail:sub(last + 1)
-    result, tail = parse_prod(tail)
-    if result == 'no match' then return result, sum_str end
-    tree.kids[#tree.kids + 1] = result
-    first, last = tail:find('^%s*%+%s*')
-  end
-  return tree, tail
+function parse_statement(str)
+  local or_parsers = {parse_assign, parse_for, parse_print}
+  return parse_or_rule(str, 'statement', or_parsers)
 end
 
-function parse_prod(prod_str)
-  local tree = {type='prod', kids={}}
-  local result, tail = parse_num(prod_str)
-  if result == 'no match' then return result, prod_str end
-  tree.kids[1] = result
-  local first, last = tail:find('^%s*%*%s*')
-  while first do
-    tail = tail:sub(last + 1)
-    result, tail = parse_num(tail)
-    if result == 'no match' then return result, prod_str end
-    tree.kids[#tree.kids + 1] = result
-    first, last = tail:find('^%s*%*%s*')
-  end
-  return tree, tail
+function parse_assign(str)
+  local or_parsers = {parse_std_assign, parse_inc_assign}
+  return parse_or_rule(str, 'assign', or_parsers)
 end
 
-function parse_num(num_str)
-  local first, last = num_str:find('^%d+')
-  if not first then return 'no match', num_str end
-  local value = tonumber(num_str:sub(first, last))
-  return {type='num', value=value}, num_str:sub(last + 1)
+function parse_std_assign(str)
+  local seq_parsers = {parse_var, parse_lit('='), parse_expr}
+  return parse_seq_rule(str, 'std_assign', seq_parsers)
+end
+
+function parse_inc_assign(str)
+  local seq_parsers = {parse_var, parse_lit('+='), parse_expr}
+  return parse_seq_rule(str, 'inc_assign', seq_parsers)
+end
+
+function parse_expr(str)
+  local or_parsers = {parse_var, parse_num}
+  return parse_or_rule(str, 'expr', or_parsers)
+end
+
+parse_var = parse_re('[A-Za-z_][A-Za-z0-9_]*', 'var')
+
+parse_num = parse_re('[1-9][0-9]*', 'num')
+
+function parse_for(str)
+  local seq_parsers = {parse_lit('for'), parse_var, parse_lit('='), parse_expr,
+                       parse_lit('to'), parse_expr, parse_lit(':'), parse_statement}
+  return parse_seq_rule(str, 'for', seq_parsers)
+end
+
+function parse_print(str)
+  local seq_parsers = {parse_lit('print'), parse_expr}
+  return parse_seq_rule(str, 'print', seq_parsers)
 end
 
 
@@ -167,14 +224,14 @@ local f = assert(io.open(in_file, 'r'))
 for line in f:lines() do
   print('line:')
   print(line)
-  local tree, tail = parse_sum(line)
+  local tree, tail = parse_statement(line)
   print('')
   print('tree:')
   pr(tree)
   print('tail:')
   pr(tail)
-  print('exec value:')
-  print(exec_tree(tree))
+  --print('exec value:')
+  --print(exec_tree(tree))
   print('')
 end
 f:close()
